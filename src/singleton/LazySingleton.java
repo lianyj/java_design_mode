4554d5758552614df317cdf98baa6af1a66108a6
lianyj <631258447@qq.com>
2018-01-02 16:35:41 +0800

    Initial commit

---
+++ src/singleton/LazySingleton.java
@@ -0,0 +1,32 @@
+package singleton; /**************************************************************************
+ * Copyright (c) 2016-2017 Zhejiang TaChao Network Technology Co.,Ltd.
+ * All rights reserved.
+ *
+ * 项目名称：浙江踏潮-基础架构
+ * 版权说明：本软件属浙江踏潮网络科技有限公司所有，在未获得浙江踏潮网络科技有限公司正式授权
+ *           情况下，任何企业和个人，不能获取、阅读、安装、传播本软件涉及的任何受知
+ *           识产权保护的内容。                            
+ ***************************************************************************/
+
+/**
+ * 懒汉式在调用取得实例方法的时候才会实例化对象
+ *
+ * @author <a href="mailto:LYJ@zjtachao.com">LYJ</a>
+ * @since 2.0
+ */
+public class LazySingleton {
+
+    private static LazySingleton lazySingleton;
+
+    private LazySingleton(){}
+    //  懒汉有线程安全问题
+    public static synchronized LazySingleton getInstance(){
+        if (lazySingleton==null){
+            lazySingleton = new LazySingleton();
+        }
+        return lazySingleton;
+    }
+
+
+
+}
